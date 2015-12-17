c2a8fdf6750d3091062eb47937d5943611855f23
= <simon.bruguerolle@hotmail.fr>
2015-12-17 17:03:21 +0100

    Optimization and game start configuration

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var readline = require('readline');
 var shipGenerator = require('./shipGenerator');
+var gridSize = 10;  //Size of the grid (= 10 by default)
 var alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var play = false; //Boolean to tell wether the game began or not
 
 // Stream reading interface
 input = readline.createInterface({
@@ -8,37 +10,72 @@ input = readline.createInterface({
   output: process.stdout
 });
 
-var play = true;
-
 // Stream
-console.log("Welcome on Battleships ! To begin choose the size of the grid. Must be less than 26 (10 adviced). Tap 'exit' ('e') to quit the game. \n");
+console.log("Welcome on Battleships ! To begin a 10x10 grid game type 'start' ('s').\n To change the grid size type 'settings n' with n the number of squares.\n Type 'exit' to quit the game.");
 input.on("line", function(ans){
-    if(Number(ans) == ans && ans < 26 && ans >= 5){
-      displayGrid(ans);
-      shipGenerator(ans, function(err, data){
-        if (err)
-          console.error("Ship generating error: " + err);
-        console.log(data);
-      });
-      console.log("Game started ! Tap the coordinates of the case you want to bomb (ex: A5).\nTap exit ('e') to quit the game.\n");
-      input.on("line", function(answer){
+  // We split the input if the user wants to change the grid size
+  var param = ans.split(" ");
 
-      });
-    }
-    else if(ans == "exit" || ans == "e"){
+  switch(param[0]){
+    case "start":     //If the input is start or s & the game didn't started
+    case "s":         //We start the game
+      if(!play){
+        displayGrid(gridSize, null);    //We display the grid
+        // We generate the computer's ships randomly
+        ships = shipGenerator(gridSize, function(err, data){
+          if (err)    //Error display
+          console.error("Error": err);
+          // We store the ships into array
+          var ships = data;
+        });
+        console.log("Game started ! To bomb a case, type its name. (ex: 'A5')");
+        play = true;
+      }
+      else {
+        console.error("The game already started ! ");
+      }
+      break;
+
+    case "settings":        //If the input is settings
+      if(!play){            //And the game didn't start
+        if(Number(param[1]) == param[1] && param[1] < 26 && param[1] >= 5){     //If second input argument is a int between 5 and 26
+          console.log("The changes have been saved. Your game will be on a "+param[1]+"x"+param[1]+" grid.\n  Type 'start' ('s') to begin.");
+          gridSize = param[1];    //We change the grid size
+        }
+        else {      //Else
+          console.error("Number of squares must be between 5 & 26. Try again");   //We display an error
+        }
+      }
+      else {
+        console.error("The game already started, you can't change the grid size ! ");
+      }
+      break;
+
+    case "exit":      //If exit
+      console.log("See you ! ");    //Message displayed and stream closed
       input.close();
-    }
-    else {
-      console.log("Input not recognised.");
-    }
+      break;
+
+    default:      //If no command recognised -> error displayed
+      if(play){
+        
+      }
+      else {
+        console.error("Input not recognised. Type 'start' ('s') to begin.\nType 'settings n' with n the number of squares to change the grid size.\nType 'exit' to quit.");
+      }
+      break;
+  }
 });
 
-function displayGrid(l){
+// Grid displaying function
+function displayGrid(l, hits){
   var row = "";
+  // Colomns' numbers display
   for(var k=1; k<=l; k++){
     row = row + " " + k;
   }
   console.log(row);
+  // Grid display
   for(var i=0; i<l; i++){
     row = alphabet[i];
     for(var j=0; j<l; j++){
